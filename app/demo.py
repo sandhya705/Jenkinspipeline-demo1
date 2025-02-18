26d8f8bed32811b57beb1756edb56c5942ce9eac
sandhya705 <gudivandulasandya@gmail.com>
2025-02-18 21:24:15 +0530

    Update demo.py

--- app/demo.py
+++ app/demo.py
@@ -1,1 +1,9 @@
-print("welcome to python")
+#Python progrom to find the largest number among the three input numbers
+#chnage the values of num1,num2 and num3
+# for a different result 
+num1=10
+num2=14 
+num3=12
+
+# uncomment following lines to take there numbers from user 
+
